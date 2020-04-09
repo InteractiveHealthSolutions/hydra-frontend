c7654575fd5c21eb75ff6b0af80686e3fdf002b0
shujaat.ali <=shujaat.dev@gmail.com>
2020-04-09 22:44:04 +0500

    index file

---
+++ src/views/ui/modal/index.js
@@ -0,0 +1,5 @@
+import {ModalTemplate} from './modalTemplate/ModalTemplate'
+import {ModalFormTemplate} from './modalFormTemplate/ModalFormTemplate'
+import FormDialog from './formDialog/FormDialog'
+import CustomizedDialog from './customizedDialog/CustomizedDialog'
+export { ModalTemplate, ModalFormTemplate ,FormDialog,CustomizedDialog}
