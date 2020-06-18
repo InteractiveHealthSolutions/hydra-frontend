612ce103048274a1caa940da7c36b8f00081b9bb
shujaat.ali <=shujaat.dev@gmail.com>
2020-06-18 14:16:31 +0500

    [Bug]:Spinner/loader added

--- src/views/components/patient/dataView/componentView/ComponentView.js
+++ src/views/components/patient/dataView/componentView/ComponentView.js
@@ -3,34 +3,43 @@ import { ExpendableList } from '../../../common/expendablelist/ExpendableList'
 import FormViewContainer from '../formView/FormViewContainer'
 import ListItem from '../../../../ui/listItem/ListItem'
 import styles from './componentview.module.css';
-const ComponentView = ({ phaseComponents, componentForms, handleOnClick, handleDeleteOnClick, ...props }) => {
-    return (
-        <>
-            {
-                (phaseComponents && phaseComponents.length > 0) ? phaseComponents.map((element, index) => (
-                    <ExpendableList
-                        margin="true"
-                        title={element.hydramoduleComponent.name}
-                    >
-                        <ul className={styles.ul_contaienr} >
+import { LoaderDots } from '../../../common/loader/LoaderDots'
+
+const ComponentView =
+    ({ phaseComponents,
+        componentForms,
+        handleOnClick,
+        handleDeleteOnClick,
+        ...props }) => (
+            <>
+                {
+                    (phaseComponents && phaseComponents.length > 0) ? phaseComponents.map((element, index) => (
+                        <ExpendableList
+                            margin="true"
+                            title={element.hydramoduleComponent.name}
+                        >
                             {
-                                (componentForms && componentForms.length > 0) ? componentForms.filter(data => data.component.uuid === element.componentUUID).map((el, index) => (
-                                    <ListItem
-                                        handleOnClick={handleOnClick}
-                                        completed={true}
-                                        handleDeleteOnClick={handleDeleteOnClick}
-                                        data={el}
-                                        view={true}
-                                    />
-                                )) : null
+                                componentForms && componentForms.length > 0 ?
+                                    <ul className={styles.ul_contaienr} >
+                                        {
+                                            componentForms.filter(data => data.component.uuid === element.componentUUID).map((el, index) => (
+                                                <ListItem
+                                                    handleOnClick={handleOnClick}
+                                                    completed={true}
+                                                    handleDeleteOnClick={handleDeleteOnClick}
+                                                    data={el}
+                                                    view={true}
+                                                />
+                                            ))
+                                        }
+                                    </ul>
+                                    : <LoaderDots withMargin="true" height={40} width={40} />
                             }
+                        </ExpendableList>
 
-                        </ul>
-                    </ExpendableList>
+                    )) : null
+                }
+            </>
+        )
 
-                )) : null
-            }
-        </>
-    )
-}
 export default ComponentView
